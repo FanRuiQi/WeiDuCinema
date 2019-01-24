ffe42733236353b777a5f670481534fab96093f7
FanRuiQi <3193765359@qq.com>
2019-01-24 19:10:53 +0800

    f

---
+++ app/src/main/java/com/bw/movie/Apis.java
@@ -0,0 +1,12 @@
+package com.bw.movie;
+
+/**
+ * Author: 范瑞旗
+ * Date: 2019/1/24 14:33
+ * Description: ${DESCRIPTION}
+ */
+public class Apis {
+
+    public static final String URL_LOGIN="login";
+    public static final String URL_REGISTER="registerUser";
+}
